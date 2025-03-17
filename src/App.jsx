b57df99ee1a8ba777603d43d7f86c8e99428c7cf
Rosa <rosa.harofdez@gmail.com>
2025-03-17 14:47:17 +0100

    Configuration + file creation

---
+++ src/App.jsx
@@ -0,0 +1,30 @@
+import './App.css'
+import store from './core/redux/store/store'
+import { Provider } from 'react-redux'
+import { BrowserRouter, Route, Routes} from 'react-router-dom'
+import ContactPage from './pages/contact/ContactPage'
+import DetailsPage from './pages/details/DetailsPage'
+import LoginPage from './pages/login/LoginPage'
+import ProfilePage from './pages/profile/ProfilePage'
+import SignupPage from './pages/signup/SignupPage'
+import HomePage from './pages/home/HomePage'
+
+function App() {
+
+  return (
+    <Provider store={store}>
+      <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/signup" element={<SignupPage />} />
+        <Route path="/details" element={<DetailsPage />} />
+        <Route path="/myprofile" element={<ProfilePage />} />
+        <Route path="/contact" element={<ContactPage />} />
+      </Routes>
+    </BrowserRouter>
+    </Provider>
+  )
+}
+
+export default App
